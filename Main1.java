4708abf4a372d007cae23a32aa5fe8f1808771da
IvanBurk <64144110+IvanBurk@users.noreply.github.com>
2021-08-13 17:00:54 +0300

    Add files via upload

--- Main1.java
+++ Main1.java
@@ -1,13 +1,18 @@
 public class Main1 {
     public static void main(String[] args) {
         int check = 100;
-        int enrollment = 2000;
+        int enrollment = 2120;
+        int balance = check + enrollment;
         if (enrollment > 1000) {
-            int balance = check + enrollment + ((enrollment*1)/100);
-            System.out.println(balance);
-        } else {
-            int balance = check + enrollment;
-            System.out.println(balance);
+            balance = check + enrollment + ((enrollment*1)/100);
         }
+        System.out.println(balance);
     }
 }
+//        if (enrollment > 1000) {
+//            int balance = check + enrollment + ((enrollment*1)/100);
+//            System.out.println(balance);
+//        } else {
+//            int balance = check + enrollment;
+//            System.out.println(balance);
+//        }
